036a3cb85cb63f8c2f42d2d09cea77264f68fa4e
lucass373 <78789490+lucass373@users.noreply.github.com>
2023-06-14 12:02:44 -0300

    ok

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,76 +1,35 @@
-// Importando os hooks useEffect e useState. Por definição, um Hook é um componente que te permite utilizar 
-// recursos do React
 import { useEffect, useState, BackHandler } from 'react';
-
-// Importando os componentes utilizados nessa página
 import { ActivityIndicator, StyleSheet, FlatList, StatusBar, Text, View, TouchableOpacity} from 'react-native';
-
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { RadioButton } from 'react-native-paper';
-
-// Importando a função que descreve como foi definida a navegação da aplicação
 import { useNavigation } from '@react-navigation/native';
 
-// Lodash (acessado usando-se o underscore) nos dá uma série de funções utilitárias para manipulação
-// de: Array,Collection, Date, Function, String, entre outras classes.
-import _ from 'lodash';
+import auth from '@react-native-firebase/auth';
 
-function Home() {
-  // Recuperando o componente de navegação da aplicação
-  const navigation = useNavigation();
-  // O hook useState define um componente Wrapper com o atributo interno 'state'. Ele nos traz uma série de
-  // vantagens: (1) a referência para o componente é 'const', mas o objeto internamente é mutável através do
-  // uso da função associada na declaração. 
-  // (2) Se o componente é utilizado em algum ponto da página e se ele troca o seu 'state', automaticamente
-  // as partes que a página referenciam o componente são alteradas, funcionando como se fosse uma callback. 
-  const [carregarPagina,setCarregarPagina] = useState(true);
-  // Hook para colocar o ícone animado de carga na página (quando se busca os dados de aluno com fetch)
-  const [isLoading, setLoading] = useState(false);
-  // Hook para guardar os alunos a serem apresentados. 
-  const [alunos, setAlunos] = useState([]);
-  // Hook para indicar as colunas da FlatList (tabela)
-  const [ colunas ] = useState([
-    {nome:" ",tam:'10%'},
-    {nome:"Id",tam:'15%'},
-    {nome:"Nome",tam:'40%'},
-    {nome:"Idade",tam:'15%'},
-    {nome:"Cidade",tam:'20%'}]);
-  // Hook para indicar qual é o sentido de ordenação a ser empregado
-  const [ direcao, setDirecao ] = useState('desc');
-  // Hook para indicar qual é a coluna selecionada como padrão de ordenação
-  const [ colunaSelecionada, setColunaSelecionada ] = useState("Id");
-  // Hook para indicar qual é o registro que está selecionado
-  const [ regSelecionado, setRegSelecionado ] = useState(null);
+import { GoogleSignin } from '@react-native-community/google-signin';
 
-  //-------------------------------------------------------------------------
-  // ObterAlunos: função para efetuar a carga dos alunos através do servidor
-  //-------------------------------------------------------------------------
-  const obterAlunos = async () => {
+function Home () {
+
+  GoogleSignin.configure({
+
+    webClientId:
     
-  }
-
-  //------------------------------------------------------------------------
-  // ExcluirAluno: função para efetuar a exclusão do registro selecionado 
-  //------------------------------------------------------------------------
-  const excluirAluno = async () => {
+    '260759292128-4h94uja4bu3ad9ci5qqagubi6k1m0jfv.apps.googleusercontent.com',
     
+    }); 
+  const loginGoogle = async ()=>{
+    const googleProvider = new GoogleAuthProvider();
+    const auth = getAuth();
+    const result = await signInWithPopup(auth, googleProvider);
+    const userGoogle = result.user;
+    console.log(result.user);
   }
-
-  useEffect(() => {
-   
-  }, []);
-
-  //-------------------------------------------------------------
-  // tableHeader
-  //-------------------------------------------------------------
-
-
-//-------------------------------------------------------------
-// RETORNO DA PÁGINA
-//-------------------------------------------------------------
 return (
- <View>
-    <Text>Login com google</Text>
+ <View style={styles.container}>
+  <TouchableOpacity onPress={loginGoogle}>
+  <Text>Login com google</Text>
+  </TouchableOpacity>
+    
  </View>
   );
 }
@@ -83,67 +42,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignSelf : 'center'
   },
-  tableHeader: {
-    textAlign: "center",
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#37C2D0",
-    borderTopEndRadius: 10,
-    borderTopStartRadius: 10,
-    height: 50
-  },
-  tableBottom: {
-    textAlign: "center",
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#37C2D0",
-    borderBottomEndRadius: 10,
-    borderBottomStartRadius: 10,
-    height: 20,
-    alignSelf: 'stretch',
-    paddingTop:10
-  },
-  columnHeader: {
-    textAlign: "center",
-    alignItems:"center"
-  },
-  columnHeaderTxt: {
-    textAlign: "center",
-    color: "white",
-    fontWeight: "bold",
-  },
-  tableRowBlue: {
-    textAlign: "center",
-    backgroundColor: "#F0FBFC",
-    flexDirection: "row",
-    height: 40,
-    alignItems:"center",
-  },
-  tableRow: {
-    textAlign: "center",
-    flexDirection: "row",
-    height: 40,
-    alignItems:"center",
-  },
-  columnRowTxt: {
-    textAlign: "center",
-    justifyContent: "center",
-  },
-  button: {
-    marginTop: 5,
-    height: 30,
-    width: 250,
-    backgroundColor: "#37C2D0",
-    borderRadius: 5,
-    fontSize: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    textAlign: "center",
-    color: "white",
-    fontWeight: "bold",
-  }
+  
 });
 export default Home;
 
